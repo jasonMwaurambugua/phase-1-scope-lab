592c20b76ad0aa76977526602d8a3c7eaaff531f
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-25 13:31:40 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Scope Lab
+// jason
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'bob'
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+var customerName = 'alice'
+const bestCustomer = 'alice'
+var bestCustomer;
+
+function changeLeastFavoriteCustomer() {
+  customerName = customerName.bestCustomer()
+}
+
+
+    
+
